c05fa77e9a99380cd59251c3b8b10d05cc433175
skEndX <71227122+skEndX@users.noreply.github.com>
2021-11-25 15:33:10 +0900

    Add files via upload

--- Pi-Cluster/r/system_monitor.R
+++ Pi-Cluster/r/system_monitor.R
@@ -1,1 +1,63 @@
+library(ggplot2)
+library(reshape2)
+library(scales)
+library(gridExtra)
 
+x = read.csv("/home/jeston/projects/pi-cluster/data/rpi_stats.csv")
+x = as.data.frame(x)
+x = na.omit(x)
+x = melt(x, id.vars = "date")
+
+x$rpi = ""
+x$rpi = ifelse(substr(x$variable, 1, 4) == "rpi1", "RPI 1", x$rpi)
+x$rpi = ifelse(substr(x$variable, 1, 4) == "rpi2", "RPI 2", x$rpi)
+x$rpi = ifelse(substr(x$variable, 1, 4) == "rpi3", "RPI 3", x$rpi)
+x$rpi = ifelse(substr(x$variable, 1, 4) == "rpi4", "RPI 4", x$rpi)
+
+x$typ = ""
+x$typ = ifelse(substr(x$variable, 6, 13) == "cpu_tmp", "CPU Temp", x$typ)
+x$typ = ifelse(substr(x$variable, 6, 13) == "cpu_pct", "CPU % Used",  x$typ)
+x$typ = ifelse(substr(x$variable, 6, 13) == "mem_fre", "RAM Free", x$typ)
+x$typ = ifelse(substr(x$variable, 6, 13) == "mem_pct", "RAM % Used",  x$typ)
+
+x$date = substr(x$date, 1, 19)
+x$date = as.POSIXct(x$date)
+
+x$rpi = factor(x$rpi)
+x$typ = factor(x$typ)
+
+
+g1 = ggplot(subset(x, typ == 'CPU Temp')) +
+  geom_line(aes(x = date, y = value, color = rpi), alpha = .5) +
+  scale_x_datetime("") +
+  scale_y_continuous("") +
+  scale_color_discrete("") +
+  facet_wrap(~typ, scales = "free") +
+  theme(text = element_text(family = "mono"),
+    plot.title = element_text(hjust = .5),
+    plot.subtitle = element_text(hjust = .5)) +
+  ggtitle("Raspber Pi Cluster Stats")
+
+g2 = ggplot(subset(x, typ == 'CPU % Used')) +
+  geom_line(aes(x = date, y = value/100, color = rpi), alpha = .5) +
+  scale_x_datetime("") +
+  scale_y_continuous("", labels = percent) +
+  scale_color_discrete("", guide = FALSE) +
+  facet_wrap(~typ, scales = "free") +
+  theme(text = element_text(family = "mono"),
+      plot.title = element_text(hjust = .5),
+      plot.subtitle = element_text(hjust = .5))
+
+g3 = ggplot(subset(x, typ == 'RAM % Used')) +
+  geom_line(aes(x = date, y = value/100, color = rpi), alpha = .5) +
+  scale_x_datetime("") +
+  scale_y_continuous("", labels = percent) +
+  scale_color_discrete("", guide = FALSE) +
+  facet_wrap(~typ, scales = "free") +
+  theme(text = element_text(family = "mono"),
+        plot.title = element_text(hjust = .5),
+        plot.subtitle = element_text(hjust = .5))
+
+ggsave("/home/jeston/projects/pi-cluster/output/cpu.png",
+  grid.arrange(g1, arrangeGrob(g2, g3, nrow = 1), nrow = 2),
+  width = 7, height = 5)
